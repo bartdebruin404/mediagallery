e3c5cb136104e54ec0f92b32baa01aa14e9983ac
bart <bart@axendo.nl>
2020-06-14 21:06:21 +0200

    Refactored project build

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var clean = require("gulp-clean");
 var concat = require("gulp-concat");
+
+gulp.task("clean-dist", function () {
+  return gulp.src('./dist', {read: false})
+      .pipe(clean());
+});
 
 gulp.task("sass", function () {
   return gulp
@@ -13,11 +19,17 @@ gulp.task("sass", function () {
 });
 
 gulp.task("img", function () {
+  return gulp
+    .src("./img/**/*.*")
+    .pipe(gulp.dest("./dist/img"));
+});
+gulp.task("scripts", function () {
     return gulp
-      .src("./img/**/*.*")
-      .pipe(gulp.dest("./dist/img"));
+      .src("./scripts/**/*.*")
+      .pipe(gulp.dest("./js"));
   });
 
 gulp.task("watch", function () {
   gulp.watch("./sass/**/*.scss", gulp.series('sass'));
+  gulp.watch("./scripts/**/*.js", gulp.series('scripts'));
 });
