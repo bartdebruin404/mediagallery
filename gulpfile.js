84f06d30a23d79681ba35007672940af386eb463
bart <bart@axendo.nl>
2020-06-14 15:00:01 +0200

    Added dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,17 @@ var sass = require("gulp-sass");
 var concat = require("gulp-concat");
 
 gulp.task("sass", function () {
+  return gulp
+    .src("./sass/styles.scss")
+    .pipe(concat("all.scss")) // concat the stream output in single file
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("img", function () {
     return gulp
-      .src("./sass/styles.scss")
-      .pipe(concat("all.scss")) // concat the stream output in single file
-      .pipe(sass().on("error", sass.logError))
-      .pipe(gulp.dest("./css"));
+      .src("./img/**/*.*")
+      .pipe(gulp.dest("./dist/img"));
   });
 
 gulp.task("watch", function () {
